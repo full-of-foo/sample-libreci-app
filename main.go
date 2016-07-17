c809ed8f880db6ce6ecb9c1ee2a7aedef0dac9b6
Anthony <lightweightdevelopment@gmail.com>
2016-07-17 12:24:12 +0100

    Webhook test

--- main.go
+++ main.go
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0 LOL
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,6 +44,7 @@ type Instance struct {
 }
 
 const version string = "1.0.0"
+
 
 func main() {
 	showversion := flag.Bool("version", false, "display version")
